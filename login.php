3841f8463de5bf5f728a2ad6f359c42a181ff3ed
SamuelSantosALC <Samuel.carvalho20@aluno.ce.gov.br>
2025-06-11 14:13:03 -0300

    11/06/25 14:11
    Adicionado a pasta assets, e começo da estilização do login.

--- login.php
+++ login.php
@@ -5,7 +5,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $login = $_POST['login'] ?? '';
     $senha = $_POST['senha'] ?? '';
 
-    // Busca o usuário pelo login
     $sql = "SELECT * FROM usuarios WHERE login = :login";
     $stmt = $pdo->prepare($sql);
     $stmt->bindValue(':login', $login);
@@ -25,16 +24,26 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <html>
 <head>
     <title>Login Admin</title>
-
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://unpkg.com/lucide@latest"></script>
 </head>
 <body>
+    <div class="bg-gray-200 flex flex-col items-center justify-center h-screen">
+    <img src="assets/adegacontrol_logo.png" class="w-120 h-80" alt="logo">
     <form method="POST">
+        <div class="flex flex-col items-center font-bold">
         <h1>Login Admin</h1>
         <p>Digite seu login e senha para acessar o painel administrativo.</p>
+    
         <input type="text" name="login" placeholder="Login" required>
         <input type="password" name="senha" placeholder="Senha" required>
-        <input type="submit" value="Entrar">
+        <button type="submit" class="bg-blue-400 hover:bg-blue-600 transition rounded-lg shadow-lg text-center p-4 text-white">Entrar</button>
+         <script>
+    lucide.createIcons();
+  </script>
+        </div>
     </form>
-    <button onclick="window.location.href='cadastroadmin.php'">Voltar Para a Pagina Inicial</button>
+    <button onclick="window.location.href='cadastroadmin.php'"> <- Voltar Para a Pagina Inicial</button>
+    </div>
 </body>
 </html>
